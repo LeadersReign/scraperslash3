ef6606ee431173e89c14d30a7df2775b02f3e45a
mandatedisrael <damiafo@MacBook-Air-M2.local>
2023-07-21 19:22:39 +0100

    Added function to analyse each subreddit

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import praw
-def main():
-    _client_id = str(input("Enter client id: "))
-    _client_secret = str(input("Enter client secret: "))
-    _user_agent = str(input("Enter user agent: "))
 
-    #Create a reddit instance
-    reddit = praw.Reddit(client_id=_client_id,
+#Get user input
+_client_id = str(input("Enter client id: "))
+_client_secret = str(input("Enter client secret: "))
+
+#Create a Global reddit instance (for now, user agent is hardcoded)
+reddit = praw.Reddit(client_id=_client_id,
                             client_secret=_client_secret,
-                            user_agent=_user_agent)
-    
+                            user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36')
 
-if __name__ == "__main__":
-    main()
+#Function to analyse a subreddit
+def analyseSubreddit():
+    option = str(input("Enter Subreddit to analyse: "))
+    subreddit = reddit.subreddit(option)
+    print("=====================================================")
+    print("* Subreddit: ", subreddit.display_name)
+    print("=====================================================")
+    print("* Title: ", subreddit.title)
+    print("* Description: ", subreddit.public_description)
+    print("* URL: ", "https://www.reddit.com"+subreddit.url)
+    print("* Subscribers: ", subreddit.subscribers)
+    print("* NSFW: ", subreddit.over18)
+    print("* Allow Images: ", subreddit.allow_images)
+    print("* Allow Videos: ", subreddit.allow_videos)
+
+    print("=====================================================")
+    print("* Rules: ")
+    [print("=>", rule) for rule in subreddit.rules]
+
+
+
+analyseSubreddit()
